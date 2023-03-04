2ed34b9e44c9eb00cc77f36e5f69865e8e40fddc
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-03-04 16:31:32 +0100

    Banner end + animations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,13 +11,22 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 const Banner = () => {
-	return <section className='section' id='home'>
+	return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
 		<div className="container mx-auto">
-			<div className='flex flex-col gapy-y-8 lg:flex-row lg:items-center'>
+			<div className='flex flex-col gapy-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
 				{ /*text */}
 				<div className='flex-1 text-center font-secondary lg:text-left'>
-					<h1 className='text-[55px] font-bold leading-[0.8]  lg:text-[110px]'>MAKSYM <span>SHYDLOVSKYI</span></h1>
-					<div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-1'>
+					<motion.h1
+						variants={fadeIn("up", 0.3)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.7 }}
+						className='text-[55px] font-bold leading-[0.8]  lg:text-[90px]'>MAKSYM <span>SHYDLOVSKYI</span></motion.h1>
+					<motion.div variants={fadeIn("up", 0.4)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.7 }}
+						className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-1'>
 						<span className='text-white mr-2 mb-10'>I am a </span>
 						<TypeAnimation
 							sequence={[
@@ -31,16 +40,29 @@ const Banner = () => {
 							className=" text-accent"
 							wrapper='span'
 							repeat={Infinity} />
-					</div>
-					<p className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet,
+					</motion.div>
+					<motion.p
+						variants={fadeIn("up", 0.5)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.7 }}
+						className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet,
 						consectetur adipisicing elit. Eius sunt quibusdam
 						maiores quam pariatur cumque hic rem vel sequi ex qui error,
-						fugiat eaque blanditiis vitae doloribus perspiciatis, corrupti optio.</p>
-					<div className='flex  gap-x-6 items-center mb-12 mx-auto max-w-max'>
+						fugiat eaque blanditiis vitae doloribus perspiciatis, corrupti optio.</motion.p>
+					<motion.div variants={fadeIn("up", 0.6)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.7 }}
+						className='flex gap-x-6 items-center mb-12  max-w-max mx-auto lg:mx-0'>
 						<button className='btn btn-lg'>Contact me</button>
 						<a href="#" className='text-gradient btn-link'>My Portfolio</a>
-					</div>
-					<div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+					</motion.div>
+					<motion.div initial="hidden"
+						variants={fadeIn("up", 0.8)}
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.7 }}
+						className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
 						<a href="#">
 							<FaYoutube />
 						</a>
@@ -50,16 +72,18 @@ const Banner = () => {
 						<a href="#">
 							<FaDribbble />
 						</a>
-					</div>
+					</motion.div>
 				</div>
 
-
-				{/*socials*/}
 
 				{ /*image */}
-				<div>
+				<motion.div initial="hidden"
+					variants={fadeIn("down", 0.3)}
+					whileInView={"show"}
+
+					className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] '>
 					<img src={Image} alt="" />
-				</div>
+				</motion.div>
 			</div>
 		</div >
 	</section >;
